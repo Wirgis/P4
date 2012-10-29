489337f0566a0121e8af982230173cfcaf8e0cd3
Andrius Dalinkevicius <adalinkevicius@gmail.com>
2012-10-29 17:30:45 +0200

    Add script for creating iniatial database with two tables.
    One table contains for names of countries, other - for categories.

---
+++ code/R.calc/30initDB.R
@@ -0,0 +1,49 @@
+library(RSQLite)
+
+## creat database
+drv <- SQLite()
+con <- dbConnect(drv, dbname = "P4.sqlite")
+
+## create table1: Countries
+cn <- read.csv("data/rawdata/EMI_private/Country.id.csv",
+               stringsAsFactors = FALSE)
+colnames(cn)[2] <- "id_cn"
+## max(nchar(cn[, "Country"]))
+
+dbGetQuery(con, "CREATE Table Countries (Country CHARACTER VARYING(15),
+id_cn SMALLINT)")
+
+sql <- "INSERT INTO Countries VALUES ($Country, $id_cn)"
+dbBeginTransaction(con)
+dbGetPreparedQuery(con, sql, bind.data = cn)
+dbCommit(con)
+
+## create table2: Categories
+cat <- read.csv("data/rawdata/EMI_private/Category.id.csv",
+                stringsAsFactors = FALSE)
+cat[, "Subcategory"] <- gsub("^([ \t\n\r\f\v]+)", "", cat[, "Subcategory"])
+##max(nchar(cat[, "Subcategory"]))
+##max(nchar(cat[, "Category"]))
+
+dbGetQuery(con, "CREATE Table Categories (id_cat SMALLINT,
+Category CHARACTER VARYING(35), Subcategory CHARACTER VARYING(90))")
+
+
+colnames(cat)[3] <- "id_cat"
+sql <- "INSERT INTO Categories VALUES ($id_cat, $Category, $Subcategory)"
+dbBeginTransaction(con)
+dbGetPreparedQuery(con, sql, bind.data = cat)
+dbCommit(con)
+
+##
+## dbListTables(con)
+## dbListFields(con, "Categories")
+## dbListFields(con, "Countries")
+
+## dbGetQuery(con, "SELECT * FROM Categories WHERE id_cat = 1")
+## dbGetQuery(con, "SELECT Country FROM Countries")
+## dbGetQuery(con, "DROP TABLE Countries")
+
+dbDisconnect(con)
+
+
