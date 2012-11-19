93faac98ff727c8a6e42695f6eb045c81f0b4c15
Andrius Dalinkevicius <adalinkevicius@gmail.com>
2012-11-19 17:48:34 +0200

    Some exploration.
    Correlation matrices within some countries.

--- code/R.calc/10code.R
+++ code/R.calc/10code.R
@@ -37,3 +37,44 @@ SetLayout <- function(ncol, nrow){
 }
 
 subplot <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
+
+InsertCorr <- function(data, sql.base, indexes, con, range1, range2){
+    foreach(i = 1:dim(indexes)[2]) %do%{
+        print(i)
+        cor <- cor(data[indexes[, i][1], -c(1:2)],
+                   data[indexes[, i][2], -c(1:2)], method = "kendall",
+                   use = "complete.obs")
+        if(range1 <= abs(cor) & abs(cor) <= range2) {
+            sql <- paste(sql.base, "(",paste(data[indexes[, i][1], "Country"],
+                                             data[indexes[, i][1],
+                                                  "CategorySub"],
+                                             data[indexes[, i][2], "Country"],
+                                             data[indexes[, i][2],
+                                                  "CategorySub"],
+                                             Corr = round(cor, 2), sep = ","),
+                         ")", sep = "")
+            dbGetQuery(con, sql)
+        }
+    }
+}
+
+
+InsertCorr2 <- function(data1, data2, sql.base, con, range1, range2){
+    for(i in 1:dim(data1)[1]){
+        for(j in 1:dim(data2)[1]){
+            print(paste(i, j))
+            cor <- cor(data1[i, -c(1, 2)], data2[j, -c(1, 2)],
+                       method = "kendall", use = "complete.obs")
+            if(range1 <= abs(cor) & abs(cor) <= range2) {
+                sql <- paste(sql.base, "(",paste(data1[i, "Country"],
+                                                 data1[i, "CategorySub"],
+                                                 data2[j, "Country"],
+                                                 data2[j, "CategorySub"],
+                                                 Corr = round(cor, 2),
+                                                 sep = ","),
+                             ")", sep = "")
+                dbGetQuery(con, sql)
+            }
+        }
+    }
+}
